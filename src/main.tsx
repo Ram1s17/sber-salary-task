29fd34ec8ec7c5479dd493c0c620ad0823418360
Ramis <ramismubick@gmail.com>
2024-05-18 11:49:16 +0300

    Подключение шрифта. styled components. Глобальные стили

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './index.css'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Benzin', Arial, sans-serif;
+  }
+`
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
 )
